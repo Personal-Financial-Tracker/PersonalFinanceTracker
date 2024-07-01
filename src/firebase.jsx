1ab028237871dbd4178f1a5844b6bef5e2d9bbf7
shashank285verma <shashank285verma@gmail.com>
2024-07-02 03:35:14 +0530

    basic structure + firebase added

---
+++ src/firebase.jsx
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBo3TiTTCEgdk34kKoqv4eWB0K-dK4CLwM",
+  authDomain: "financely-b0d83.firebaseapp.com",
+  projectId: "financely-b0d83",
+  storageBucket: "financely-b0d83.appspot.com",
+  messagingSenderId: "383671515685",
+  appId: "1:383671515685:web:dd5c94ae1a2876672f1dcc",
+  measurementId: "G-EK738N9D0F"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+export { db, auth, provider, doc, setDoc };
